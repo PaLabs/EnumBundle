c35177c73d4d6e9fb063b09d78e869896d8364fd
lewbor <boris.alperin@gmail.com>
2021-12-24 12:07:30 +0700

    Add symfony6 support

--- src/PaEnumBundle.php
+++ src/PaEnumBundle.php
@@ -7,11 +7,12 @@ namespace PaLabs\EnumBundle;
 use PaLabs\EnumBundle\DependencyInjection\PaEnumExtension;
 use PaLabs\EnumBundle\Doctrine\DoctrineEnumCacheLoader;
 use PaLabs\EnumBundle\Initializer\EnumInitializer;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class PaEnumBundle extends Bundle
 {
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         return new PaEnumExtension();
     }
